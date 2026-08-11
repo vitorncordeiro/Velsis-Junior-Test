74bce294d0f00c30276a6adeee80c167a031e6fe
Vitor Natal <vitornc31@gmail.com>
2026-08-11 09:35:15 -0300

    build: initializes the project

---
+++ src/main/java/com/example/versis/VersisApplication.java
@@ -0,0 +1,13 @@
+package com.example.versis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/versis/VersisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
