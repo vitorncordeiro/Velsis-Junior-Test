12a844951d318355b0ea8cd904cfc3ac0e140d6c
vitornc <vitornc31@gmail.com>
2026-08-12 18:23:21 -0300

    refactor: create backend dir and move all packages into

--- backend/src/main/java/com/example/velsis/dto/response/UserResponse.java
+++ backend/src/main/java/com/example/velsis/dto/response/UserResponse.java
@@ -1,6 +1,7 @@
 package com.example.velsis.dto.response;
 
 public record UserResponse(
+        Long id,
         String username,
         String email
 ) {
--- backend/src/main/java/com/example/velsis/mapper/UserMapper.java
+++ backend/src/main/java/com/example/velsis/mapper/UserMapper.java
@@ -14,6 +14,7 @@ public class UserMapper {
     }
     public static UserResponse modelToResponse(UserModel userModel){
         return new UserResponse(
+                userModel.getId(),
                 userModel.getUsername(),
                 userModel.getEmail()
         );
